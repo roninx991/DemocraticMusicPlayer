6e311ac7bd19b08233113e3573e0cfe4d0f213c4
Sangat Das <sangatdas5@gmail.com>
2017-11-11 14:11:17 +0530

    Class Based Views

--- DMP/DMP/urls.py
+++ DMP/DMP/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
-from songs.views import home,home1,home2,home3
+from songs.views import HomeView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$',home),
-    url(r'^home1/$',home1),
-    url(r'^home2/$',home2),
-    url(r'^home3/$',home3),
+    url(r'^$',HomeView.as_view()),
+    url(r'^about/$',TemplateView.as_view(template_name="about.html")),
+    url(r'^contact/$',TemplateView.as_view(template_name="contact.html")),
 ]
--- DMP/songs/views.py
+++ DMP/songs/views.py
@@ -1,46 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from django.views.generic import TemplateView
 import random
 # Create your views here.
 # Function based view
 
-def home(request):
-	num = random.randint(0,1000)
-	some_list = [num,random.randint(0,500),random.randint(0,3000)]
-	context = {
-		"bool_item" : True,
-		"num" : num,
-		"some_list" : some_list}
-	#return HttpResponse(html_)
-	return render(request,"base.html",context)#response 
+class HomeView(TemplateView):
+	template_name = "home.html"
+	def get_context_data(self, *args, **kwargs):
+		context = super(HomeView,self).get_context_data(*args,**kwargs)
+		num = None
+		bool_item = True
+		if bool_item :
+			num = random.randint(0,3500)
+		some_list = [
+			random.randint(0,10000),
+			random.randint(0,20000),
+			random.randint(0,1000),
+			]
+		context = {
+			"some_list" : some_list,
+			"num" : num,
+			}
+		return context
 
-def home1(request):
-	num = random.randint(0,1000)
-	some_list = [num,random.randint(0,500),random.randint(0,3000)]
-	context = {
-		"bool_item" : True,
-		"num" : num,
-		"some_list" : some_list}
-	#return HttpResponse(html_)
-	return render(request,"home1.html",context)#response 
-
-def home2(request):
-	num = random.randint(0,1000)
-	some_list = [num,random.randint(0,500),random.randint(0,3000)]
-	context = {
-		"bool_item" : True,
-		"num" : num,
-		"some_list" : some_list}
-	#return HttpResponse(html_)
-	return render(request,"home2.html",context)#response 
-
-def home3(request):
-	num = random.randint(0,1000)
-	some_list = [num,random.randint(0,500),random.randint(0,3000)]
-	context = {
-		"bool_item" : True,
-		"num" : num,
-		"some_list" : some_list}
-	#return HttpResponse(html_)
-	return render(request,"home3.html",context)#response 
 

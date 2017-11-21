9e171e2972983707d03ff28b77768e661910e8ef
Sangatdas <Sangatdas5@gmail.com>
2017-11-22 01:34:38 +0530

    Uploading run.sh and setup.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+pip install -r requirements.txt
+python DMP/manage.py runserver
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+#Setting up of important components
+echo "#############################Setting up of important components###################################################"
+sudo apt-get install virtualenv
+virtualenv -p python3 .
+
